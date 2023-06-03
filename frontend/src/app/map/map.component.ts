6a105352e230c87d145816df81113bf5a8118769
lauraghimici <98102824+lauraghimici@users.noreply.github.com>
2023-06-03 16:44:51 +0200

    icon on map

--- frontend/src/app/map/map.component.ts
+++ frontend/src/app/map/map.component.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { IconLayer } from '@deck.gl/layers';
 import { MapboxOverlay as DeckOverlay } from '@deck.gl/mapbox';
-import * as mapboxgl from 'mapbox-gl';
+import mapboxgl from 'mapbox-gl';
 import axios from 'axios';
 
 const OPENSKY_API_URL = 'https://opensky-network.org/api/states/all';
-const REFRESH_INTERVAL = 86400000; // a day
+const REFRESH_INTERVAL = 5000 * 60 * 60; // 5 seconds
 
 interface FlightData {
   time: number;
@@ -22,11 +22,12 @@ export class MapComponent implements OnInit {
   private intervalId: any;
 
   ngOnInit(): void {
-    (mapboxgl as typeof mapboxgl).accessToken =
+    mapboxgl.accessToken =
       'pk.eyJ1IjoibGF1cmFnaCIsImEiOiJjbGk4cThscnMxdjY0M2VtbDc3Yjdsa25wIn0.S3BdCi6irPxokf4rJcGBMQ';
+
     const map = new mapboxgl.Map({
       container: 'map',
-      style: 'mapbox://styles/mapbox/outdoors-v11', // Use a 2D map style
+      style: 'mapbox://styles/mapbox/outdoors-v11',
       center: [0.45, 51.47],
       zoom: 4,
     });
@@ -35,15 +36,13 @@ export class MapComponent implements OnInit {
       layers: [],
     });
 
-    map.on('load', () => {
-      map.addControl(this.deckOverlay);
-      map.addControl(new mapboxgl.NavigationControl());
+    map.addControl(this.deckOverlay);
+    map.addControl(new mapboxgl.NavigationControl());
 
-      this.fetchFlightData(); // Fetch flight data initially
-      this.intervalId = setInterval(() => {
-        this.fetchFlightData(); // Fetch flight data periodically
-      }, REFRESH_INTERVAL);
-    });
+    this.fetchFlightData(); // Fetch flight data initially
+    this.intervalId = setInterval(() => {
+      this.fetchFlightData(); // Fetch flight data periodically
+    }, REFRESH_INTERVAL);
   }
 
   ngOnDestroy(): void {
@@ -70,16 +69,26 @@ export class MapComponent implements OnInit {
     }));
     return latestData;
   }
+
   private updateMap(data: any[]): void {
     const layers = [
       new IconLayer({
         id: 'aircraft',
         data: data,
         pickable: true,
-        iconAtlas: '/assets/airplane.svg',
-        getSize: () => 20,
+        opacity: 1,
+        iconAtlas: '/assets/airplane.svg', // Replace with the path to your aircraft icons atlas
+        iconMapping: {
+          airplane: { x: 0, y: 0, width: 100, height: 100, mask: false },
+        }, // Adjust the mapping according to your aircraft icons atlas
+        sizeScale: 15,
         getPosition: (d: any) => [d.coordinates.lon, d.coordinates.lat],
+        getIcon: () => 'airplane',
+        getSize: () => 1,
         getColor: () => [255, 140, 0],
+        outline: true,
+        outlineWidth: 1,
+        outlineColor: [0, 0, 0],
       }),
     ];
 
