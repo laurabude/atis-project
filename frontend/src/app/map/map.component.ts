074a7d722ebe3b5bf3479d615e952b12eb9cd406
lauraghimici <98102824+lauraghimici@users.noreply.github.com>
2023-06-06 20:52:47 +0200

    Popup works

--- frontend/src/app/map/map.component.ts
+++ frontend/src/app/map/map.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import mapboxgl, { LngLatLike } from 'mapbox-gl';
-import { Feature, Point, Geometry } from 'geojson';
-import { Socket, io } from 'socket.io-client';
+import { Feature, Point } from 'geojson';
+import axios from 'axios';
 
-const OPENSKY_WEBSOCKET_URL = 'wss://opensky-network.org/api/states/all';
-const MAPBOX_ACCESS_TOKEN = 'pk.eyJ1IjoibGF1cmFnaCIsImEiOiJjbGk4cThscnMxdjY0M2VtbDc3Yjdsa25wIn0.S3BdCi6irPxokf4rJcGBMQ';
+const OPENSKY_API_URL = 'https://opensky-network.org/api/states/all';
+const MAPBOX_ACCESS_TOKEN =
+  'pk.eyJ1IjoibGF1cmFnaCIsImEiOiJjbGk4cThscnMxdjY0M2VtbDc3Yjdsa25wIn0.S3BdCi6irPxokf4rJcGBMQ';
 
 @Component({
   selector: 'app-map',
@@ -13,13 +14,13 @@ const MAPBOX_ACCESS_TOKEN = 'pk.eyJ1IjoibGF1cmFnaCIsImEiOiJjbGk4cThscnMxdjY0M2Vt
 })
 export class MapComponent implements OnInit {
   private map!: mapboxgl.Map;
-  private socket!: Socket;
+  private popup!: mapboxgl.Popup;
 
   ngOnInit(): void {
     mapboxgl.accessToken = MAPBOX_ACCESS_TOKEN;
 
     this.initializeMap();
-    this.initializeWebSocket();
+    this.fetchFlightData();
   }
 
   private initializeMap(): void {
@@ -32,67 +33,85 @@ export class MapComponent implements OnInit {
 
     this.map.on('load', () => {
       this.map.addControl(new mapboxgl.NavigationControl());
-    });
-  }
 
-  private initializeWebSocket(): void {
-    this.socket = io(OPENSKY_WEBSOCKET_URL);
-
-    this.socket.on('message', (data: any) => {
-      const flightData = JSON.parse(data);
-      this.displayFlightData(flightData);
-    });
-  }
-
-  private displayFlightData(data: any): void {
-    const airplanes: Feature<Point, {
-      heading: number;
-      callSign: string;
-      altitude: number;
-      speed: number;
-    }>[] = data.states.map(
-      (flightArr: any) =>
-        ({
-          type: 'Feature',
-          geometry: {
-            type: 'Point',
-            coordinates: [flightArr[5], flightArr[6]],
-          },
-          properties: {
-            heading: flightArr[10] || 0,
-            callSign: flightArr[1] || '',
-            altitude: flightArr[7] || 0,
-            speed: flightArr[9] || 0,
-          },
-        } as Feature<Point, {
-          heading: number;
-          callSign: string;
-          altitude: number;
-          speed: number;
-        }>)
-    );
-
-    const source = this.map.getSource('airplanes') as mapboxgl.GeoJSONSource;
-    if (source) {
-      source.setData({
-        type: 'FeatureCollection',
-        features: airplanes,
+      this.map.loadImage('/assets/airplane.png', (error, image) => {
+        if (error) {
+          console.error('Error loading the airplane icon image:', error);
+        } else {
+          this.map.addImage('airplane-icon', image);
+        }
       });
-    } else {
-      this.map.addSource('airplanes', {
-        type: 'geojson',
-        data: {
+
+      this.map.on('click', 'airplane-layer', (event) => {
+        const coordinates = (
+          event.features[0].geometry as Point
+        ).coordinates.slice();
+        const { callSign, altitude, speed } = event.features[0].properties;
+
+        // Ensure that the popup is not already open
+        if (this.popup && this.popup.isOpen()) {
+          this.popup.remove();
+        }
+
+        this.popup = new mapboxgl.Popup({ closeButton: false })
+          .setLngLat(coordinates as LngLatLike)
+          .setHTML(
+            `<div>
+              <strong>Call Sign:</strong> ${callSign}<br>
+              <strong>Altitude:</strong> ${altitude} ft<br>
+              <strong>Speed:</strong> ${speed} knots
+            </div>`
+          )
+          .addTo(this.map);
+      });
+
+      // Change the cursor style when hovering over an airplane marker
+      this.map.on('mouseenter', 'airplane-layer', () => {
+        this.map.getCanvas().style.cursor = 'pointer';
+      });
+
+      // Change the cursor style back to default when not hovering over an airplane marker
+      this.map.on('mouseleave', 'airplane-layer', () => {
+        this.map.getCanvas().style.cursor = '';
+      });
+    });
+  }
+
+  private async fetchFlightData(): Promise<void> {
+    try {
+      const response = await axios.get(OPENSKY_API_URL);
+      const flightData = response.data.states;
+      const airplanes: Feature<Point>[] = flightData.map(
+        (flightArr: any) =>
+          ({
+            type: 'Feature',
+            geometry: {
+              type: 'Point',
+              coordinates: [flightArr[5], flightArr[6]],
+            },
+            properties: {
+              callSign: flightArr[1] || '',
+              altitude: flightArr[7] || 0,
+              speed: flightArr[9] || 0,
+            },
+          } as Feature<Point>)
+      );
+
+      const source = this.map.getSource('airplanes') as mapboxgl.GeoJSONSource;
+      if (source) {
+        source.setData({
           type: 'FeatureCollection',
           features: airplanes,
-        },
-      });
-    }
-
-    this.map.loadImage('/assets/airplane.png', (error, image) => {
-      if (error) {
-        console.error('Error loading the airplane icon image:', error);
+        });
       } else {
-        this.map.addImage('airplane-icon', image);
+        this.map.addSource('airplanes', {
+          type: 'geojson',
+          data: {
+            type: 'FeatureCollection',
+            features: airplanes,
+          },
+        });
+
         this.map.addLayer({
           id: 'airplane-layer',
           type: 'symbol',
@@ -101,11 +120,12 @@ export class MapComponent implements OnInit {
             'icon-image': 'airplane-icon',
             'icon-size': 0.02,
             'icon-allow-overlap': true,
-            'icon-rotate': ['get', 'heading'],
           },
           paint: {},
         });
       }
-    });
+    } catch (error) {
+      console.error('Error fetching flight data:', error);
+    }
   }
 }
